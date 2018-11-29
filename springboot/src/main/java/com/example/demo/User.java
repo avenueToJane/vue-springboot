f6efd46c262ba9f1fe05b557db9bec3316379615
杨生 <289536143@qq.com>
2018-11-29 19:54:22 +0800

    Initial commit

---
+++ springboot/src/main/java/com/example/demo/User.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+/**
+ * @Author: yangsheng
+ * @Description:
+ * @Date: Created in 下午 06:54 2018-11-29
+ */
+public class User {
+    //用户名
+    private String userName;
+    //密码
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
